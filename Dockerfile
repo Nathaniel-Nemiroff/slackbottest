f93b8558bfd9244fbabc9321eb1370fe327132e1
nate <nate000088@gmail.com>
2019-04-12 06:04:11 -0700

    this is all you really need

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,18 @@
 FROM python:2.7
+RUN pip install slackclient
+
+#RUN curl -Lo /ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip;unzip -o /ngrok.zip -d ./
+#RUN  ./ngrok authtoken 2BRrdymMATW3q4B8keykC_5PvpojiLFyeUDoS86Cvqa
+
 
 WORKDIR /server
 COPY . /server
 
-
 EXPOSE 8000
-ENV NAME sbt
+#EXPOSE 4040
+ENV NAME ab
 
-RUN python server.py &
-RUN curl localhost:8000
-#CMD ['python', 'server.py']
+#CMD ["ngrok","http", "4567", "&"]
+#CMD ["curl", "localhost:4040/api/tunnels"]
+RUN echo '';echo '-----------------------';echo 'REMEMBER TO:';echo ' 1: Ensure your slackclient token is correct'; echo ' 2: expose 8000 to the world';echo '-----------------------';echo ''
+CMD ["python", "server.py"]
--- server.py
+++ server.py
@@ -1,11 +1,43 @@
+#!/usr/bin/env python
 import SimpleHTTPServer
 import SocketServer
+import os
+import time
+import re
+from slackclient import SlackClient
 
-PORT = 8000
+import logging
+logging.basicConfig()
+import requests
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+print 'MAKE SURE TOKEN IS IN SERVER.PY'
+slack_client = SlackClient('[TOKEN]')
+slackbot_id = None
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+def sendMsg(msg):
+    if slack_client.rtm_connect(with_team_state=False):
+        print("nemi bot connected and running.")
+        slackbot_id = slack_client.api_call("auth.test")["user_id"]
+        slack_client.api_call(
+            "chat.postMessage",
+            channel='#botchannel',
+            text=msg
+            )
 
-print "serving at port", PORT
-httpd.serve_forever()
+class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        print 'asdf'
+        sendMsg('from simple http')
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+    def do_POST(self):
+
+        content_length = int(self.headers['Content-Length']) 
+        post_data = self.rfile.read(content_length) 
+        sendMsg(post_data)
+        print post_data
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+Handler = MyRequestHandler
+server = SocketServer.TCPServer(('0.0.0.0', 8000), Handler)
+
+server.serve_forever()
